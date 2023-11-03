2d6571f7924522e4c0bd52d99dd89b7a9abeb200
Antoniio-Orozco <91105692+Antoniio-Orozco@users.noreply.github.com>
2023-11-03 15:01:53 -0600

    Add files via upload
    v.1.0

---
+++ vistas/modulos1/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2022 <a href="https://www.google.com/" target="_blank">SmartByte Sistemas</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
